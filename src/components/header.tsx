1a37e9dbc92d99d9bf714204a2c044d27b0d0003
Bia Miranda <Bia@Macs-MacBook-Pro.local>
2024-04-07 23:30:50 -0300

    initial commit

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import nlwUniteIcon from '../assets/nlw-unite-icon.svg';
+import { NavLink } from './nav-link';
+
+export function Header() {
+    return (
+        <div className="flex items-center gap-5 py-2">
+            <img src={nlwUniteIcon} />
+            <NavLink href="/eventos">Eventos</NavLink>
+            <NavLink href="/participantes">Participantes</NavLink>
+        </div>
+    )
+}
